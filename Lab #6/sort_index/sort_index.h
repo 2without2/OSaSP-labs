57730af3f4bd7afcde1a0bb1850ce693d923e463
maxim265 <95981853+maxim265@users.noreply.github.com>
2024-05-06 22:55:30 +0300

    Add files via upload

---
+++ Lab #6/sort_index/sort_index.h
@@ -0,0 +1,15 @@
+
+#ifndef SORT_INDEX_SORT_INDEX_H
+#define SORT_INDEX_SORT_INDEX_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/resource.h>
+
+#define MAX_THREADS 8192
+#define MIN_THREADS 8
+
+#endif //SORT_INDEX_SORT_INDEX_H
